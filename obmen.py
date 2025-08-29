37013b69a1b65f3321980fe0640bd0f9b239f513
yuliyamikhaylovskaya1989-blip <yuliyamikhaylovskaya1989@gmail.com>
2025-08-29 15:48:19 +0300

    создадлии функцию для аолчения курса валют

--- obmen.py
+++ obmen.py
@@ -6,10 +6,25 @@ from  tkinter import messagebox as mb
 
 
 def exchange():
+    code=entry.get()
+    if code:
+        try:
+            response=requests.get('https://open.er-api.com/v6/latest/usd')
+            response.raise_for_status()
+            data=response.json()
+            if code in data['rates']:
+                exchange_rate=data['rates'][code]
+                mb.showinfo("Курс обмена",f'Курс: {exchange_rate} {code} за 1 доллар')
+            else:
+                mb.showerror("Ошибка!",f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f'Произошла ошибка: {e}.')
+    else:
+        mb.showwarning("Внимание!","Введите код валюты!")
 
 window=Tk()
 window.title("Курсы обмена валюты")
-window.geometry("360x80")
+window.geometry("360x140")
 
 Label(text="Введите код валюты").pack(padx=10,pady=10)
 
@@ -21,7 +36,3 @@ window.mainloop()
 
 
 
-result=requests.get('https://open.er-api.com/v6/latest/usd')
-data=json.loads(result.text)
-p=pprint.PrettyPrinter(indent=4)
-p.pprint(data)
